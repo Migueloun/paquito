8a59048d9d295f0674de33f88dfdee299104d359
u49253 <u49253@s001-n015.aidevcloud>
2020-10-12 17:38:39 -0700

    Test

--- 12_10/mmult0.py
+++ 12_10/mmult0.py
@@ -1,6 +1,12 @@
 import numpy as np
 
 if __name__ == "__main__":
-    N = 3
-    A = np.random.randn(N, N)
     
+    N = 3
+    
+    A = np.random.randn(N, N)
+    B = np.identity(N)
+    C = np.zeros((N, N))
+    
+    for row in range(0, len(A)):
+        
