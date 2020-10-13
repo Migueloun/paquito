ea934fb50fb55fdce6e3690136553d15f6917b2d
u49253 <u49253@s001-n015.aidevcloud>
2020-10-12 18:24:00 -0700

    2020-10-12 18:23:59

--- 12_10/pmmult0.py
+++ 12_10/pmmult0.py
@@ -1,6 +1,19 @@
 import numpy as np
 from multiprocessing import shared_memory, Process
 
+def matrix_multiply(row):
+    
+    global N
+    
+    global A
+    global B
+    global C
+    
+    
+    for col in range(0, N, 1):
+        for k in range(0, N, 1):
+            C[row][col] += 
+            
 if __name__ == "__main__":
     
     N = 3
@@ -8,7 +21,7 @@ if __name__ == "__main__":
     A = np.random.randn(N, N)
     B = np.identity(N)
     
-    ptr = shared_memory.SharedMemory(name='C', create=True, size=A.nbytes)
+    ptr = shared_memory.SharedMemory(create=True, size=A.nbytes)
     C = np.ndarray(B.shape, dtype=np.float64, buffer=ptr.buf)
     
     print(A)
@@ -16,7 +29,12 @@ if __name__ == "__main__":
     
     jobs = []
     for row in range(0, N):
-        p = Process()
+        p = Process(target=, args=())
+        jobs.append(p)
+        p.start()
+        
+    for p in jobs:
+        p.join()
     
     """
     for row in range(0, len(A)):
