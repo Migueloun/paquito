3adcb7c353f74bdf1e5ce687460fb0eef8d011b1
u49253 <u49253@s001-n002.aidevcloud>
2020-10-19 18:26:46 -0700

    2020-10-19 18:26:46

--- smmult/main.c
+++ smmult/main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define N 3
+
 int main( int argc, char * argv [] )
 {
     //uint32_t num = 5;
@@ -11,6 +13,20 @@ int main( int argc, char * argv [] )
     //printf("Resultado: %d\n", num);
     
     
+    float A[N][N] = {};
+    uint8_t i=0;
+    uint8_t j=0;
+        
+    srand(time(NULL));
+    
+    for(i=0; i<N; i++)
+    {
+        for(j=0; j<N; j++)
+        {
+            A[i][j] = (float)rand()/(float)(RAND_MAX/10);
+            printf("%f\n", A[i][j]);
+        }
+    }                
 
     return 0;
 }
