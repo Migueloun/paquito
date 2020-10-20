b0cd7f7a15ded79633775cc5f0dc91f33f701cbb
u49253 <u49253@s001-n002.aidevcloud>
2020-10-19 18:12:59 -0700

    2020-10-19 18:12:59

--- smmult/Libraries/include/myLibrary.h
+++ smmult/Libraries/include/myLibrary.h
@@ -1,3 +1,8 @@
+#ifndef MYLIBRARY_H
+#define MYLIBRARY_H
+
 #include <inttypes.h>
 
 extern uint32_t suma_uno(uint32_t num);
+
+#endif
--- smmult/main.c
+++ smmult/main.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-#include <inttypes.h>
-
 #include "myLibrary.h"
-
-uint32_t suma_uno(uint32_t num)
-{
-    return num+1;
-}
+#include <stdlib.h>
+#include <time.h>
 
 int main( int argc, char * argv [] )
 {
-    uint32_t num = 5;
+    //uint32_t num = 5;
     
-    num = suma_uno(num);
-    printf("Resultado: %d\n", num);
+    //num = suma_uno(num);
+    //printf("Resultado: %d\n", num);
+    
+    
 
     return 0;
 }
